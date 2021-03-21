78b72f591ea7df5f09ad7ac4bfacde1c5a76d834
krystianpetek <krystianpetek2@gmail.com>
2021-03-21 16:43:55 +0100

    initial commit

---
+++ SecondAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
