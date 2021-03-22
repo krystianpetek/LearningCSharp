5744f2ffedbcd364e133f7c2f53988c30e434e28
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-03-22 20:34:25 +0100

    wyłączony prąd commit

--- SecondAPP/Program.cs
+++ SecondAPP/Program.cs
@@ -70,6 +70,24 @@ namespace SecondAPP
             int fahrenheit = 94;
             decimal oblicz = (fahrenheit - 32m) * 5m / 9m;
             Console.WriteLine($"The temperature is {oblicz} Celcius");
+
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine($"Wylosowana liczba to: {roll}");
+
+            int number = 7;
+            string text = "seven";
+            Console.WriteLine(number);
+            Console.WriteLine();
+            Console.WriteLine(text);
+
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(101);
+            int roll3 = dice.Next(50, 101);
+
+            Console.WriteLine($"First roll: {roll1}");
+            Console.WriteLine($"Second roll: {roll2}");
+            Console.WriteLine($"Third roll: {roll3}");
         }
     }
 }
