1c698e18e81206a905e0d815f66c2870326a2c91
krystianpetek <krystianpetek2@gmail.com>
2021-03-23 20:20:10 +0100

    pierwsze próby

--- FifthAPP_tablice/Program.cs
+++ FifthAPP_tablice/Program.cs
@@ -6,7 +6,22 @@ namespace FifthAPP_tablice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            /*
+            string[] fraudulentOrderIDs = new string[3];
+            fraudulentOrderIDs[0] = "A123";
+            fraudulentOrderIDs[1] = "B456";
+            fraudulentOrderIDs[2] = "C789";
+            //fraudulentOrderIDs[3] = "D000";
+            */
+            string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+            Console.WriteLine($"Pierwsze: {fraudulentOrderIDs[0]}");
+            Console.WriteLine($"Drugie: {fraudulentOrderIDs[1]}");
+            Console.WriteLine($"Trzecie: {fraudulentOrderIDs[2]}");
+
+            fraudulentOrderIDs[0] = "F000";
+            Console.WriteLine($"Ponowne przypisanie Pierwsze: {fraudulentOrderIDs[0]}");
+            Console.WriteLine($"Mamy {fraudulentOrderIDs.Length} fałszywe zamówienia do sprawdzenia.");
         }
     }
 }
