91561c3373e978f87cf996e72908709202b3c553
krystianpetek <krystianpetek2@gmail.com>
2021-03-23 21:47:59 +0100

    final Fifth

--- FifthAPP_tablice/Program.cs
+++ FifthAPP_tablice/Program.cs
@@ -22,6 +22,32 @@ namespace FifthAPP_tablice
             fraudulentOrderIDs[0] = "F000";
             Console.WriteLine($"Ponowne przypisanie Pierwsze: {fraudulentOrderIDs[0]}");
             Console.WriteLine($"Mamy {fraudulentOrderIDs.Length} fałszywe zamówienia do sprawdzenia.");
+
+            string[] names = { "Bob", "Conrad", "Grant" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            int[] inventory = { 200, 450, 700, 175, 250 };
+            int suma = 0;
+            int pojemnik = 0;
+            foreach (int items in inventory)
+            {
+                pojemnik++;
+                suma += items;
+                Console.WriteLine($"Pojemnik {pojemnik} = {items} przedmiotów (W sumie: {suma})");
+            }
+            Console.WriteLine($"Mamy {suma} przedmiotów w ekwipunku\n");
+
+            string[] falszyweZamowienia = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+            foreach (string i in falszyweZamowienia)
+            {
+                if(i.StartsWith("B"))
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
