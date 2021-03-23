7d36400f8753825ad13c37e0ca46ddf0cc536cff
krystianpetek <krystianpetek2@gmail.com>
2021-03-23 17:18:47 +0100

    first commit

---
+++ ThirdAPP_Decision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThirdAPP_Decision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
