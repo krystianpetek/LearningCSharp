6097295f5f4da2b6e83111b6fbbd89bf6ed1240c
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 18:36:58 +0100

    final

--- ConsoleApp1_Wyrazenia/Program.cs
+++ ConsoleApp1_Wyrazenia/Program.cs
@@ -6,7 +6,35 @@ namespace ConsoleApp1_Wyrazenia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("a" == "a");
+            Console.WriteLine("a" == "A");
+            Console.WriteLine(1 == 2);
+
+            string myValue = "a";
+            Console.WriteLine(myValue == "a");
+
+            string value1 = " a";
+            string value2 = "A ";
+            Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+            Console.WriteLine("a" != "a");
+            Console.WriteLine("a" != "A");
+            Console.WriteLine(1 != 2);
+            myValue = "a";
+            Console.WriteLine(myValue != "a");
+
+            Console.WriteLine(1 > 2);
+            Console.WriteLine(1 < 2);
+            Console.WriteLine(1 >= 1);
+            Console.WriteLine(1 <= 1);
+
+            string pangram = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine(pangram.Contains("fox"));
+            Console.WriteLine(pangram.Contains("cow"));
+
+            pangram = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine(!pangram.Contains("fox"));
+            Console.WriteLine(!pangram.Contains("cow"));
         }
     }
 }
