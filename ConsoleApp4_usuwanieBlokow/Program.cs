733d2e1352aa250a672315bf3d32db05a973bd6c
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 22:32:31 +0100

    initial i final

---
+++ ConsoleApp4_usuwanieBlokow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp4_usuwanieBlokow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            if (flag) Console.WriteLine(flag);
+
+            string name = "steve";
+            if (name == "bob")
+                Console.WriteLine("Found Bob");
+            else if (name == "steve")
+                Console.WriteLine("Found Steve");
+            else
+                Console.WriteLine("Found Chuck");
+
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number == 42) found = true;
+            }
+            
+            if(found) Console.WriteLine("Set contains 42");
+            
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
