329ff082383c2b5ccab249e4d9f1cedf300864ca
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 20:32:35 +0100

    final commit

--- ConsoleApp2_zadanie/Program.cs
+++ ConsoleApp2_zadanie/Program.cs
@@ -15,11 +15,62 @@ namespace ConsoleApp2_zadanie
 
             Random losowanie = new Random();
             int szansa = losowanie.Next(0, 100);
-            Console.WriteLine("Jeżeli wylosowane zostanie 0-50 to reszka, od 51 do 100 orzeł");
             Console.WriteLine($"Liczba: {szansa}: {(szansa>=50 ? "orzeł":"reszka")}");
 
             szansa = losowanie.Next(0, 2);
-            Console.WriteLine($"Liczba: {szansa}: {(szansa == 0 ? "orzeł" : "reszka")}");
+            Console.WriteLine($"Liczba: {szansa}: {(szansa > 0 ? "orzeł" : "reszka")}");
+
+            string permission = "Admin|Manager";
+            int level = 55;
+            bool checkAdmin = permission.Contains("Admin");
+            bool checkManager = permission.Contains("Manager");
+            if (checkAdmin && level > 55)
+            {
+                Console.WriteLine("Welcome, Super Admin user");
+            }
+            else if (checkAdmin && level <= 55)
+            {
+                Console.WriteLine("Welcome, Admin user");
+            }
+            else if (checkManager && level >= 20)
+            {
+                Console.WriteLine("Contact an Admin for access.");
+            }
+            else if (checkManager && level < 20)
+            {
+                Console.WriteLine("You do not have sufficient privileges.");
+            }
+            else if(!checkAdmin || !checkManager)
+            {
+                Console.WriteLine("You do not have sufficient privileges. NULL");
+            }
+
+            if(permission.Contains("Admin"))
+            {
+                if(level > 55)
+                {
+                    Console.WriteLine("Welcome, Super Admin user");
+                }
+                else
+                {
+                    Console.WriteLine("Welcome, Admin user");
+                }
+            }
+            else if(permission.Contains("Manager"))
+            {
+                if(level >=20 )
+                {
+                    Console.WriteLine("Contact an Admin for access.");
+                }
+                else
+                {
+                    Console.WriteLine("You do not have sufficient privileges.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You do not have sufficient privileges.");
+            }
         }
     }
 }
