d1f0493de1450831a3d3a1e8f7e8cbfc4ae6b06b
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 18:56:00 +0100

    rozwiązanie 2

--- ConsoleApp2_zadanie/Program.cs
+++ ConsoleApp2_zadanie/Program.cs
@@ -12,10 +12,14 @@ namespace ConsoleApp2_zadanie
             Console.WriteLine($"Zniżka: {znizka}zł");
 
             Console.WriteLine($"Zniżka: {(wartoscSprzedazy = 900 > 1000 ? 100 : 50)}zł");
-        }
-        static void program()
-        {
 
+            Random losowanie = new Random();
+            int szansa = losowanie.Next(0, 100);
+            Console.WriteLine("Jeżeli wylosowane zostanie 0-50 to reszka, od 51 do 100 orzeł");
+            Console.WriteLine($"Liczba: {szansa}: {(szansa>=50 ? "orzeł":"reszka")}");
+
+            szansa = losowanie.Next(0, 2);
+            Console.WriteLine($"Liczba: {szansa}: {(szansa == 0 ? "orzeł" : "reszka")}");
         }
     }
 }
