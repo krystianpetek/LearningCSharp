402203bf6dc21cb42625726c455610d0baf2f0ca
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 22:33:17 +0100

    first commit initial ConsoleApp5

---
+++ ConsoleApp5_switch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp5_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
