17c64b678052fd251771f5e96ea05c7e6717a238
krystianpetek <krystianpetek2@gmail.com>
2021-03-25 18:37:12 +0100

    initial

---
+++ ConsoleApp2_zadanie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int wartoscSprzedazy = 1001;
+            int znizka = wartoscSprzedazy > 1000 ? 100 : 50;
+            Console.WriteLine($"Zniżka: {znizka}zł");
+
+            Console.WriteLine($"Zniżka: {(wartoscSprzedazy = 900 > 1000 ? 100 : 50)}zł");
+        }
+        static void program()
+        {
+
+        }
+    }
+}
