6512b48906188295d970815d57338d91bf28b017
krystianpetek <krystianpetek2@gmail.com>
2021-03-27 10:47:36 +0100

    Update Program.cs

--- ConsoleApp6_for/Program.cs
+++ ConsoleApp6_for/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp6_for
         static void Main(string[] args)
         {
             int i;
-            for (i=0; i<10;i++)
+            for (i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
             }
