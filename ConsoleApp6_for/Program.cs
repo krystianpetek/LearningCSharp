f48c307b1798ed7694638998276251fed101d98a
krystianpetek <krystianpetek2@gmail.com>
2021-03-27 09:20:52 +0100

    initial commit CA6

---
+++ ConsoleApp6_for/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp6_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i=0; i<10;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
