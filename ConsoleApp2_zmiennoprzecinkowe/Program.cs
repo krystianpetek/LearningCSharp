dfa68f4518f9fc2c310fc6e756145f787fc59d09
krystianpetek <krystianpetek2@gmail.com>
2021-03-29 23:41:41 +0200

    commit

--- ConsoleApp2_zmiennoprzecinkowe/Program.cs
+++ ConsoleApp2_zmiennoprzecinkowe/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApp2_zmiennoprzecinkowe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Typy zmiennoprzecinkowe:");
+            Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} z dokładnością 6-9 cyfr");
+            Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} z dokładnością 15-17 cyfr");
+            Console.WriteLine($"decimal  : {decimal.MinValue} to {decimal.MaxValue} z dokładnością 28-29 cyfr");
         }
     }
 }
