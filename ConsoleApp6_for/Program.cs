08828288ffe38723223bfbcd05129606d6a383a6
krystianpetek <krystianpetek2@gmail.com>
2021-03-29 20:33:58 +0200

    for final

--- ConsoleApp6_for/Program.cs
+++ ConsoleApp6_for/Program.cs
@@ -11,6 +11,48 @@ namespace ConsoleApp6_for
             {
                 Console.WriteLine(i);
             }
+
+            Console.WriteLine();
+            for ( i = 10; i >= 0; i--)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+            for ( i = 0; i < 10; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 7) break;
+            }
+
+            Console.WriteLine();
+            string[] names = { "Alex", "Eddie", "David", "Michael" };
+            for (i = names.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine();
+            for (int j = 0; j < names.Length; j++)
+            {
+                Console.WriteLine(names[j]);
+            }
+
+            Console.WriteLine();
+            for (i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "David")
+                    names[i] = "Sammy";
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
