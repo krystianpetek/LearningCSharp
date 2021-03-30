f57599131eff6848386eb033587ed471278465d8
krystianpetek <krystianpetek2@gmail.com>
2021-03-30 18:30:14 +0200

    final?

--- ConsoleApp2_zmiennoprzecinkowe/Program.cs
+++ ConsoleApp2_zmiennoprzecinkowe/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp2_zmiennoprzecinkowe
             Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} z dokładnością 6-9 cyfr");
             Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} z dokładnością 15-17 cyfr");
             Console.WriteLine($"decimal  : {decimal.MinValue} to {decimal.MaxValue} z dokładnością 28-29 cyfr");
+            Console.WriteLine("5E+2 = 500 ponieważ jest to 5 * 10^2");
         }
     }
 }
