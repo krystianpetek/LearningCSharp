479e16959603097dc1e0e568eef625fb8e97b2bd
krystianpetek <krystianpetek2@gmail.com>
2021-04-04 23:29:00 +0200

    OBJETOSC STOZKA IN PROGRESS

---
+++ ConsoleApp101_objetoscStozka/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp101_objetoscStozka
+{
+    class Program
+    {
+        /*66,6*/
+        static void Main1(string[] args)
+        {
+            string wejscie = Console.ReadLine();
+            string[] linia = wejscie.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int R = int.Parse(linia[0]);
+            int L = int.Parse(linia[1]);
+            if (R > L)
+            {
+                Console.WriteLine("obiekt nie istnieje");
+            }
+            else if (R < 0 || L < 0)
+            {
+                Console.WriteLine("ujemny argument");
+            }
+            else
+            {
+                const double PI = 3.14;
+                double h = Math.Sqrt(Math.Pow(L, 2) - Math.Pow(R, 2));
+                double polePodstawy = PI * Math.Pow(R, 2);
+
+                double Obj = (polePodstawy * h) / 3;
+                int a = (int)Obj;
+                int b = Convert.ToInt32(Obj);
+                Console.WriteLine($"{a} {b}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string wejscie = Console.ReadLine();
+            string[] linia = wejscie.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int R = int.Parse(linia[0]);
+            int L = int.Parse(linia[1]);
+            if (R > L)
+            {
+                Console.WriteLine("obiekt nie istnieje");
+            }
+            else if (R < 0 || L < 0)
+            {
+                Console.WriteLine("ujemny argument");
+            }
+            /*else if (R == 0 || L == 0)
+            {
+                throw new ArgumentException("błędne dane");
+            }*/
+            else
+            {
+                const double PI = 3.14;
+                double h = Math.Sqrt(Math.Pow(L, 2) - Math.Pow(R, 2));
+                double polePodstawy = PI * Math.Pow(R, 2);
+
+                double Obj = (polePodstawy * h) / 3;
+
+                int a = (int)Obj;
+                int b = (int)Obj + 1;
+                Console.WriteLine("Convert" + Convert.ToSingle(Obj));
+                Console.WriteLine("rzutowanie" + (float)Obj);
+                Console.WriteLine($"{a} {b}");
+            }
+        }
+    }
+}
