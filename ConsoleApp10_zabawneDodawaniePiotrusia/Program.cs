9802cf7286947ccb0e48a54de14c3edbe36284b7
krystianpetek <krystianpetek2@gmail.com>
2021-04-06 00:07:46 +0200

    ConsoleApp10_zabawneDodawaniePiotrusia in progress

---
+++ ConsoleApp10_zabawneDodawaniePiotrusia/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp10_zabawneDodawaniePiotrusia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i = 0;i<t;i++)
+            {
+                int liczba = int.Parse(Console.ReadLine());
+
+
+                
+
+                if (liczba > 0 && liczba < 81)
+                {
+                    if (liczba.ToString().Length == 1)
+                    {
+                        Console.WriteLine($"{liczba} 0");
+                        continue;
+                    }
+
+                    string lewa = liczba.ToString().Substring(0, 1);
+                    string prawa = liczba.ToString().Substring(1,1);
+                    if(lewa == prawa)
+                    {
+                        Console.WriteLine($"{liczba} 0");
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("blad");
+                }
+            }
+        }
+    }
+}
