1bf5b4a530bbfdb35f91ebdd55edad71e3a31980
krystianpetek <krystianpetek2@gmail.com>
2021-04-05 20:47:48 +0200

    ConsoleApp101_objetoscStozka in progress

--- ConsoleApp101_objetoscStozka/Program.cs
+++ ConsoleApp101_objetoscStozka/Program.cs
@@ -37,7 +37,15 @@ namespace ConsoleApp101_objetoscStozka
             string[] linia = wejscie.Split(" ", StringSplitOptions.RemoveEmptyEntries);
             int R = int.Parse(linia[0]);
             int L = int.Parse(linia[1]);
-            if (R > L)
+            if(R > 1000000 || R < -1000000)
+            {
+                throw new ArgumentException("błędne dane");
+            }
+            if (L > 1000000 || L < -1000000)
+            {
+                throw new ArgumentException("błędne dane");
+            }
+            else if (R > L)
             {
                 Console.WriteLine("obiekt nie istnieje");
             }
@@ -45,23 +53,40 @@ namespace ConsoleApp101_objetoscStozka
             {
                 Console.WriteLine("ujemny argument");
             }
+            else if(R == 0 && L > 0)
+            {
+                Console.WriteLine("obiekt nie istnieje");
+            }
             /*else if (R == 0 || L == 0)
             {
                 throw new ArgumentException("błędne dane");
             }*/
             else
             {
-                const double PI = 3.14;
+                const double PI = 3.141592653589793238462643383279;
                 double h = Math.Sqrt(Math.Pow(L, 2) - Math.Pow(R, 2));
                 double polePodstawy = PI * Math.Pow(R, 2);
 
                 double Obj = (polePodstawy * h) / 3;
 
-                int a = (int)Obj;
-                int b = (int)Obj + 1;
-                Console.WriteLine("Convert" + Convert.ToSingle(Obj));
-                Console.WriteLine("rzutowanie" + (float)Obj);
-                Console.WriteLine($"{a} {b}");
+                int wynik = (int)Math.Round(Obj);
+                int wynik2 = (int)(Obj);            
+
+
+                if(wynik2 == wynik && R != L)
+                {
+                    wynik++;
+                }
+
+                if(wynik>wynik2)
+                {
+                    int temp = wynik;
+                    wynik = wynik2;
+                    wynik2 = temp;
+                }
+                Console.WriteLine($"{wynik} {wynik2}" );
+
+                
             }
         }
     }
