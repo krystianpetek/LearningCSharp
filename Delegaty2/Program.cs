26b55447d4f9ac38bb46b71f02b09b92520d1e67
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-06-14 09:26:42 +0200

    Delegaty

---
+++ Delegaty2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegaty2
+{
+    class Obliczenia
+    {
+        public delegate void Operacje(Obliczenia O);
+        private readonly int X;
+        private readonly int Y;
+        public Obliczenia(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Dodawanie()
+        {
+            Console.WriteLine(X + Y);
+        }
+        public void Odejmowanie()
+        {
+            Console.WriteLine(X - Y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Obliczenia(5, 3);
+            var aa = new Obliczenia.Operacje(a);
+        }
+    }
+}
