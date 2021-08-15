f6f6245efab65d23ac06d5ea851abaf7c5239830
krystianpetek <krystianpetek2@gmail.com>
2021-08-15 14:28:40 +0200

    commit

---
+++ Rozdzial3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
