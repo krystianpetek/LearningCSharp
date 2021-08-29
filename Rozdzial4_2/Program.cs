27d8ba595b4e407240e5eb0118dce8a583d01ee2
krystianpetek <krystianpetek2@gmail.com>
2021-08-29 20:39:26 +0200

    Update Program.cs

--- Rozdzial4_2/Program.cs
+++ Rozdzial4_2/Program.cs
@@ -19,8 +19,55 @@ namespace Rozdzial4_2
                 Console.Write(temp + " ");
             }
             Console.WriteLine($"Następna liczba fibonacciego to {current}");
-        
-            
+
+            // FOR
+            for (int x = 0, y = 5; ((x <= 5) && (y >= 0)); y--, x++)
+            {
+                Console.Write($"{ x }{ ((x > y) ? '>' : '<')}{ y }\t");
+            }
+
+            // FOREACH
+            char[] cells = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            Console.Write("Możliwe posunięcia to: ");
+            // Wyświetlanie posunięć dopuszczalnych na początku.
+            cells[1] = 'X';
+            foreach (char cell in cells)
+            {
+                if (cell != 'O' && cell != 'X')
+                {
+                    Console.Write($"{ cell } ");
+                }
+            }
+            Console.WriteLine();
+
+            // SWITCH
+
+            bool valid;
+            Console.Write("Wprowadz liczbę: 1 - 9: ");
+            string input2 = Console.ReadLine();
+            switch (input2)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    valid = true;
+                    Console.WriteLine("OK");
+                    break;
+                case "":
+                case "quit":
+                    valid = true;
+                    break;
+                default:
+                    Console.WriteLine("BŁĄD: Wprowadź wartość z przedziału od 1 do 9. " + "Wciśnij ENTER, by zamknąć program");
+                    break;
+            }
+
         }
 
     }
