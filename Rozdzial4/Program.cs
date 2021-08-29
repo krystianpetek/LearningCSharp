9f36bdea381bceafc53bfca9e48ab196267706fc
krystianpetek <krystianpetek2@gmail.com>
2021-08-29 12:44:24 +0200

    commiot

--- Rozdzial4/Program.cs
+++ Rozdzial4/Program.cs
@@ -226,6 +226,14 @@ namespace Rozdzial4
                 mask >>= 1;
             }
             Console.WriteLine();
+            // SYSTEM.CONVERT.TOSTRING(VALUE,2)
+
+            // bitowe przypisania
+            and = 12; or = 12; xor = 12;
+            and &= 7; // and = 4
+            or |= 7; // or = 15
+            xor ^= 7; // xor = 11
+            Console.WriteLine($"and = { and } \nor = { or }\nxor = { xor }");
         }   
     }
 }
---
+++ Rozdzial4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
