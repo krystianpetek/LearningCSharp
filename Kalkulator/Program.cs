f08fe5ba8c3f798532c2ab78bdcc4f8316f37920
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-08-31 15:58:17 +0200

    Update Program.cs

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -6,7 +6,33 @@ namespace Kalkulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= i;x++)
+            {
+                string[] wejscie = Console.ReadLine().Split(" ");
+                int? obliczenia = null;
+
+                switch (wejscie[0])
+                {
+                    case "+":
+                        obliczenia = int.Parse(wejscie[1]) + int.Parse(wejscie[2]);
+                        break;
+                    case "-":
+                        obliczenia = int.Parse(wejscie[1]) - int.Parse(wejscie[2]);
+                        break;
+                    case "*":
+                        obliczenia = int.Parse(wejscie[1]) * int.Parse(wejscie[2]);
+                        break;
+                    case "/":
+                        obliczenia = int.Parse(wejscie[1]) / int.Parse(wejscie[2]);
+                        break;
+                    case "%":
+                        obliczenia = int.Parse(wejscie[1]) % int.Parse(wejscie[2]);
+                        break;
+                }
+                Console.WriteLine(obliczenia);
+            }
+
         }
     }
 }
