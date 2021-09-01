23ea2df3627c317adf203a83c3dcdb9d70b0d6dd
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-09-01 12:01:23 +0200

    Update Program.cs

--- Rozdzial5/Program.cs
+++ Rozdzial5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using static System.Console; // opcjonalny using
+
+using CountDownTimer = System.Timers.Timer; // ALIAS
 
 namespace Rozdzial5
 {
@@ -15,10 +18,13 @@ namespace Rozdzial5
             fullName = GetFullName(imie, nazwisko);
             inicjaly = GetInitials(imie, nazwisko);
             Przywitanie(fullName,inicjaly);
-            
+            var NAME = GetName();
+            Console.WriteLine(NAME);
+            Console.WriteLine( GetFullName2("krystian", "petek") );
+
         }
         static string GetUserInput(string prompt)
-        {
+        {   
             Console.Write(prompt);
             return Console.ReadLine().Split(" ")[0];
         }
@@ -34,8 +40,22 @@ namespace Rozdzial5
         {
             Console.WriteLine($"Witaj {fullName}! Twoje inicjały to: {inicjaly}"); 
         }
-
+        static (string name, string surname, int age) GetName() 
+        {
+            string firstNAME, lastNAME; int age;
+            firstNAME = "Zastosowano";
+            lastNAME = "KROTKI";
+            age = 23; 
+            return (firstNAME, lastNAME, age); // zwracanie wielu wartości za pomocą krotki
+        }
         // REFAKTORYZACJA - służy do zmniejszania liczby powtórzeń w kodzie, bo metode można wywołać wiele razy
+
+        // LAMBDA - metoda z ciałem w postaci wyrażenia
+        static string GetFullName2 (string firstname, string lastname) => $"{firstname} {lastname}";
         
+        // ALIAS USING System.Timers.Timer;
+        CountDownTimer timer; // alias
+
+
     }
 }
