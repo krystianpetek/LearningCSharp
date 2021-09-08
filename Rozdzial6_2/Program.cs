e019424f01ec07e29c7ce5cd9b62dce896040d6d
krystianpetek <krystianpetek2@gmail.com>
2021-09-08 19:41:33 +0200

    2

---
+++ Rozdzial6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
