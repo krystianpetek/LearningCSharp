8127ad71273e57c0ec5ee4c8b9c107338aacbc9e
krystianpetek <krystianpetek2@gmail.com>
2021-09-09 20:03:58 +0200

    Update Program.cs

--- Rozdzial6_3/Program.cs
+++ Rozdzial6_3/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Rozdzial6_3
 {
+    class Employee
+    {
+        // Inicjatory obiektów
+        public Employee(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+        private string _imie;
+        public string imie
+        {
+            get { return _imie; }
+            set { _imie = value; }
+        }
+        private string _nazwisko;
+
+        public string nazwisko
+        {
+            get { return _nazwisko; }
+            set { _nazwisko = value; }
+        }
+        public string Title { get; set; }
+        public string? Salary { get; set; }
+
+        // inicjatory kolekcji
+        // 278
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee employee1 = new Employee("Inigo", "Montoya") {Title = "Maniak komputerowy", Salary = "Za niskie" };
         }
     }
 }
