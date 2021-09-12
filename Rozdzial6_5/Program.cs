27efd602a7309afa6fdfda56657b8d0f7decd1dd
krystianpetek <krystianpetek2@gmail.com>
2021-09-12 17:29:59 +0200

    commit

--- Rozdzial6_5/Program.cs
+++ Rozdzial6_5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Rozdzial6_5
 {
@@ -19,17 +20,53 @@ namespace Rozdzial6_5
             NextId++;
         }
     }
+
+    // METODY STATYCZNE
+    public static class DirectoryInfoExtension
+    {
+        public static void CopyTo(DirectoryInfo sourceDirectory, string target, SearchOption option, string searchPattern)
+        {
+            if(target[target.Length - 1] != Path.DirectorySeparatorChar) // jeśli na ostatniej pozycji nie ma separatora, to ma go dopisać "\"
+            {
+
+                target += Path.DirectorySeparatorChar;
+            }
+            if(!Directory.Exists(target)) // sprawdza czy jest folder o takiej nazwie, jesli nie to go tworzy
+            {
+                Directory.CreateDirectory(target);
+            }
+            for(int i = 0;i<searchPattern.Length;i++) 
+            {
+                foreach(string file in Directory.GetFiles(sourceDirectory.FullName,searchPattern))
+                {
+                    File.Copy(file, target + Path.GetFileName(file), true);
+                }
+            }
+            // REKURENCYJNE KOPIOWANIE PODKATALOGÓW
+            if(option == SearchOption.AllDirectories)
+            {
+                foreach(string element in Directory.GetDirectories(sourceDirectory.FullName))
+                {
+                    File.Copy(element, target+Path.GetFileName(element),true);
+                }
+            }
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Employee.NextId = 1000000;
-
             Employee employee1 = new Employee("Inigo", "Montoya");
             Employee employee2 = new Employee("Princes", "Buttercup");
             Console.WriteLine("{0} {1} ({2})", employee1.FirstName, employee1.LastName, employee1.Id);
             Console.WriteLine("{0} {1} ({2})", employee2.FirstName, employee2.LastName, employee2.Id);
             Console.WriteLine($"NextId = {Employee.NextId}");
+
+            DirectoryInfo directory = new DirectoryInfo(".\\Source");
+            directory.MoveTo(".\\Root");
+            DirectoryInfoExtension.CopyTo(directory, ".\\Target", SearchOption.AllDirectories, "*");
         }
     }
 }
