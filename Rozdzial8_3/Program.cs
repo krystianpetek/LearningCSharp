bfc24b809cc8882113c836458d930ade0c614828
krystianpetek <krystianpetek2@gmail.com>
2021-09-19 12:47:30 +0200

    x

--- Rozdzial8_3/Program.cs
+++ Rozdzial8_3/Program.cs
@@ -2,11 +2,63 @@
 
 namespace Rozdzial8_3
 {
+    // dziedziczenie interfejsów
+    interface IReadableSettingsProvider
+    {
+        string GetSetting(string name, string defaultValue);
+    }
+    interface IWriteableSettingsProvider
+    {
+        void SetSetting(string name, string value);
+    }
+
+    // dziedziczenie po wielu interfejsach
+    interface ISettingsProvider : IReadableSettingsProvider, IWriteableSettingsProvider
+    {
+
+    }
+    class FileSettingsProvider : ISettingsProvider
+    {
+        #region Składowe z interfejsu ISettingsProvider
+        public void SetSetting(string name, string value)
+        {
+
+        }
+        #endregion
+
+        #region Składowe z interfejsu IReadableSettingsProvider
+        public string GetSetting(string name, string defaultValue)
+        {
+            return "";
+        }
+        #endregion
+    }
+
+    // tworzenie interfejsu pochodnego od innego interfejsu, ponieważ nie zaleca się dodawania nowych składowych do udostępnionego już interfejsu
+    interface IDistributedSettingsProvider : ISettingsProvider
+    {
+        /// <summary>
+        /// Pobieranie ustawień dla konkretnego identyfikatora URI.
+        /// </summary>
+        /// <param name="machineName">Identyfikator uri, którego dotyczą ustawienia</param>
+        /// <param name="name">NAzwa ustawienia.</param>
+        /// <param name="defaultValue">Wartość zwracana, gdy nie mozna znaleźć danego ustawienia</param>
+        /// <returns>Określone ustawienie</returns>
+        string GetSetting(string machineName, string name, string defaultValue);
+        /// <summary>
+        /// Określenie ustawienia dla konkretnego identyfikatora URI
+        /// </summary>
+        /// <param name="machineName">Identyfikator uri, którego dotyczy ustawienie</param>
+        /// <param name="name">Nazwa ustawienia</param>
+        /// <param name="value">Zapisywana wartość</param>
+        void SetSetting(string machineName, string name, string value);
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //356
+
         }
     }
 }
---
+++ Rozdzial8_4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Rozdzial8_4
+{
+    // Interfejsy w C# 8.0 - nowe cechy
+    public interface IListable
+    {
+        string?[] CellValues { get; }
+        ConsoleColor[] CellColors
+        {
+            get
+            {
+                var result = new ConsoleColor[CellValues.Length];
+                Array.Fill(result, DefaultColumnColor);
+                return result;
+            }
+        }
+        public static ConsoleColor DefaultColumnColor { get; set; }
+    }
+    public class Contact : IListable
+    {
+        public string[] CellValues
+        {
+            get
+            {
+                return new string[]
+                {
+                    FirstName, LastName, Phone, Address
+                };
+            }
+        }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string? Phone { get; set; }
+        string? Address { get; set; }
+        // brak implementacji cellColors
+    }
+
+    public class Publication : IListable
+    {
+        string?[] IListable.CellValues
+        {
+            get
+            {
+                return new string?[]
+                {
+                    Title,Author,Year.ToString()
+                };
+            }
+        }
+        ConsoleColor[] IListable.CellColors
+        {
+            get
+            {
+                string?[] columns = ((IListable)this).CellValues;
+                ConsoleColor[] result = ((IListable)this).CellColors;
+                if (columns[YearIndex]?.Length != 4)
+                {
+                    result[YearIndex] = ConsoleColor.Red;
+                }
+                return result;
+            }
+        }
+        string Title { get; set; }
+        string Author { get; set; }
+        string Year { get; set; }
+        int YearIndex = 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    // Mechanizmy wprowadzone w C# 8.0
+    // Składowe statyczne
+    public interface ISampleInterface
+    {
+        private static string? _Field;
+        public static string? Field { get => _Field; private set => _Field = value; }
+        static ISampleInterface() => Field = "Nelsona Mandela";
+        public static string? GetField() => Field;
+    }
+}
