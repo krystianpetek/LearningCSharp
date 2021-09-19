b22731447007d6c2f5bfaeb3017ee3c4900f7c0a
krystianpetek <krystianpetek2@gmail.com>
2021-09-19 14:59:43 +0200

    x

---
+++ Rozdzial8_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
