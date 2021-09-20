4719b98b45d03cb103ed9a5a2a7dec39557ad49d
krystianpetek <krystianpetek2@gmail.com>
2021-09-20 23:13:23 +0200

    Update Program.cs

--- Rozdzial9/Program.cs
+++ Rozdzial9/Program.cs
@@ -10,4 +10,31 @@ namespace Rozdzial9
 
         }
     }
+    // Do deklarowania typów bezpośrednich służy słowo kluczowe struct.
+    struct Angle
+    {
+        public Angle(int degrees, int minutes, int seconds)
+        {
+            Degrees = degrees;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public int Degrees { get; }
+        public int Minutes { get; }
+        public int Seconds { get; }
+        
+        public Angle Move(int degrees, int minutes, int seconds)
+        {
+            return new Angle(Degrees + degrees, Minutes + minutes, Seconds + seconds);
+        }
+    }
+
+    // Deklaracja klasy, czyli typu referencyjnego.
+    // Użycie w deklaracji słowa kluczowego struct spowodowałoby powstanie typu bezpośredniego zajmującego więcej niż 16 bajtów.
+    class Coordinate
+    {
+        public Angle Longitude { get; set; }
+        public Angle Latitude { get; set; }
+    }
+    //384
 }
