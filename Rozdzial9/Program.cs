41d457348afbd5c8c7404bc202fc61e1839dedfc
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-09-20 15:03:48 +0200

    9

---
+++ Rozdzial9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
