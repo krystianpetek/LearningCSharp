4d6789df51c95e45ba909d9b013693a31aca70f7
krystianpetek <krystian.petek@microsoft.wsei.edu.pl>
2021-09-22 14:22:29 +0200

    Update Program.cs

--- Rozdzial9_3/Program.cs
+++ Rozdzial9_3/Program.cs
@@ -2,16 +2,37 @@
 
 namespace Rozdzial9_3
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
 
     // PRZECIĄŻAJ operatory równości (Equals(), == i !=) w typach bezpośrednich, jeśli sprawdzanie równości jest istotne.
 
     // OPAKOWYWANIE
-    //388
+    class DisplayFibonacci
+    {
+        static void Main()
+        {
+            int totalCount;
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+            Console.WriteLine("Wprowadź liczbę z przedziału od 2 do 1000: ");
+            totalCount = int.Parse(Console.ReadLine());
+
+            if (totalCount == 7)
+            {
+
+            }
+            else
+            {
+                list.Add((double)0);
+            }
+            list.Add((double)0);
+            list.Add((double)1);
+            for(int count = 2;count<totalCount;count++)
+            {
+                list.Add((double)list[count - 1]! + (double)list[count - 2]!);
+            }
+            foreach(double count in list)
+            {
+                Console.Write($"{count}, ");
+            }
+        }
+    }
 }
