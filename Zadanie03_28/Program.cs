5d2817533cf6ba2b4e73ea1cb59c0f538b4d332b
krystianpetek <krystianpetek2@gmail.com>
2021-10-03 18:36:54 +0200

    ROZDZIAL3 FINAL I COMMIT 4KI

--- Zadanie03_28/Program.cs
+++ Zadanie03_28/Program.cs
@@ -6,7 +6,16 @@ namespace Zadanie03_28
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1 sposob
+            for(int i = 0;i<10;i++)
+            {
+                for(int j = 0;j<2;j++)
+                    Console.Write(i);
+            }
+            // 2 sposob
+            Console.WriteLine();
+            for(int z = 0;z<20;z++)
+                Console.Write(z/2);
         }
     }
 }
