b56646646977b48c7f692aafe76fc9ced2ce15d0
krystianpetek <krystianpetek2@gmail.com>
2021-10-03 19:03:56 +0200

    rozdzial4

---
+++ Zadanie04_07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Zadanie04_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] macierz = new int[10, 10];
+
+            for (int i = 0; i < 10; i++) 
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 0)
+                    {
+                        macierz[i, j] = i;
+                        Console.Write($" {macierz[i, j]} ");
+                    }
+                    else if (j == 1)
+                    {
+                        macierz[i, j] = i * i;
+                        Console.Write($" {macierz[i, j]} ");
+                    }
+                    else if(i < 4 && j == 2) // DODATKOWE dla wygladu
+                    {
+                        macierz[i, j] = 0;
+                        Console.Write($"  {macierz[i,j]} ");
+                    }
+                    else
+                    {
+                        macierz[i, j] = 0;
+                        Console.Write($" {macierz[i,j]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Zadanie04_08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie04_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 72
+        }
+    }
+}
