5b38318547536c6363bac4b70b4efdd3ba68ddb0
krystianpetek <krystianpetek2@gmail.com>
2021-10-09 19:13:51 +0200

    x

--- Zadanie05_01/Program.cs
+++ Zadanie05_01/Program.cs
@@ -6,7 +6,36 @@ namespace Zadanie05_01
     {
         static void Main(string[] args)
         {
-            // 90
+            Prostokat prosto = new Prostokat();
+            prosto.czytaj_date();
+            prosto.przetworz_dane();
+            prosto.wyswietl_dane();
+        }
+
+    }   
+    class Prostokat
+    {
+
+        double a, b, oblicz_pole;
+
+        public void czytaj_date()
+        {
+            Console.WriteLine("Program oblicza pole prostokąta");
+            Console.Write("Podaj bok a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Podaj bok b: ");
+            b = double.Parse(Console.ReadLine());
+        }
+        public void przetworz_dane()
+        {
+            oblicz_pole = a * b;
+
+        }
+        public void wyswietl_dane()
+        {
+            Console.WriteLine($"Pole prostokąta o boku a = {a} i boku b = {b} wynosi: ");
+            Console.Write($"Wynik: {oblicz_pole}");
+
         }
     }
 }
