c06ff05fa6f6cebdf53c233c1d74c25dc1563067
krystianpetek <krystianpetek2@gmail.com>
2021-10-31 22:47:17 +0100

    x

---
+++ workWithFilesCopy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace workWithFilesCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salesFiles = FindFiles("../../../stores");
+
+            foreach(var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach(var file in foundFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+            return salesFiles;
+        }
+    }
+
+}
