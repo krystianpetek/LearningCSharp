ab1d856b0ca2854bbc1727e99043ac21bc111a24
krystianpetek <krystianpetek2@gmail.com>
2021-11-04 22:10:57 +0100

    fiboi cos

---
+++ Zadanie05_05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie05_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n: ");
+            int n = int.Parse(Console.ReadLine());
+            Silnia silnia = new Silnia();
+
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine($"!{i} - { silnia.Oblicz(i)}");
+        }
+    }
+    class Silnia
+    {
+        public long Oblicz(int n)
+        {
+            if(n > 0)
+            {
+                return n * Oblicz(n - 1);
+            }
+            return 1;
+        }
+    }
+}
