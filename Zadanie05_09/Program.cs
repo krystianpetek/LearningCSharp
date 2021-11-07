60e01a4fc65001d052e3b0d4deceba4ec453cce3
krystianpetek <krystianpetek2@gmail.com>
2021-11-07 21:32:02 +0100

    Update Program.cs

--- Zadanie05_09/Program.cs
+++ Zadanie05_09/Program.cs
@@ -6,7 +6,56 @@ namespace Zadanie05_09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kadra x = new Kadra();
+            x.wczytaj1();
+            x.wyswietl1();
+        }
+    }
+    class Osoba
+    {
+        string nazwisko, imie, ulica, kod, miasto;
+        public void wczytaj()
+        {
+            Console.WriteLine("Podaj nazwisko: ");
+            nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj imie: ");
+            imie = Console.ReadLine();
+            Console.WriteLine("Podaj ulicę: ");
+            ulica = Console.ReadLine();
+            Console.WriteLine("Podaj kod pocztowy: ");
+            kod = Console.ReadLine();
+            Console.WriteLine("Podaj miasto: ");
+            miasto = Console.ReadLine();
+        }
+        public void wyswietl()
+        {
+            Console.WriteLine("Wyświetlanie danych");
+            Console.WriteLine("===================");
+            Console.WriteLine($"Nazwisko: {nazwisko}");
+            Console.WriteLine($"Imię: {imie}");
+            Console.WriteLine($"Ulica: {ulica}");
+            Console.WriteLine($"Kod: {kod}");
+            Console.WriteLine($"Miasto: {miasto}");
+        }
+    }
+    class Kadra : Osoba
+    {
+        string wyksztalcenie;
+        string stanowisko;
+        public void wczytaj1()
+        {
+            wczytaj();
+            Console.WriteLine("Podaj wykształcenie");
+            wyksztalcenie = Console.ReadLine();
+            Console.WriteLine("Podaj stanowisko");
+            stanowisko = Console.ReadLine();
+        }
+        public void wyswietl1()
+        {
+            wyswietl();
+            Console.WriteLine("Wykształcenie: " + wyksztalcenie + ".");
+            Console.WriteLine($"Stanowisko: {stanowisko}.");
+            Console.WriteLine();
         }
     }
 }
