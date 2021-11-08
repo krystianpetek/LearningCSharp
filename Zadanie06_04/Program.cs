1df475583267ee94aba6cb8b2cc805a3f59e0105
krystianpetek <krystianpetek2@gmail.com>
2021-11-08 08:32:30 +0100

    x

---
+++ Zadanie06_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie06_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
