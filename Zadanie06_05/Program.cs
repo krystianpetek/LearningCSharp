1f7f2fad201cf02db30c2733ebe873691d0282ed
krystianpetek <krystianpetek2@gmail.com>
2021-11-08 21:43:36 +0100

    koniec

---
+++ Zadanie06_05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Zadanie06_05
+{
+    class Serializacja
+    {
+        double[] pomiary = { 10.17, 12.83, 11.78, 15.23, 11.08, 13.67 };
+        public void zapisz_czytaj_dane()
+        {
+            Console.WriteLine("Wszystkie pomiary: ");
+            for (int i = 0; i < pomiary.Length; i++)
+            {
+                Console.WriteLine(pomiary[i] + " ");
+            }
+            Stream StreamWrite = new FileStream("pomiary.dat", FileMode.Create);
+
+        }
+    }
+}
