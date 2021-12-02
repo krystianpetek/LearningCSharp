d321371a0b3029b78d026409cc24243ab74de8be
krystianpetek <krystianpetek2@gmail.com>
2021-12-02 22:19:23 +0100

    Update Program.cs

--- SZTypyGeneryczne/Program.cs
+++ SZTypyGeneryczne/Program.cs
@@ -6,7 +6,19 @@ namespace SZTypyGeneryczne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // programujemy logike, ale nie przejmuijemy sie typem, robimy metody generyczne
+            Compare<string> compareStrings = new Compare<string>();
+            Compare<int> compareInts = new Compare<int>();
+        }
+        class Compare<AnyType>
+        {
+            public bool CompareTwoValues(AnyType a, AnyType b)
+            {
+                if(a.Equals(b))
+                    return true;
+                else
+                    return false;
+            }
         }
     }
 }
