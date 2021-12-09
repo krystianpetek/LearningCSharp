802a13b046d64c7c721005fa35e928e729421bcc
krystianpetek <krystianpetek2@gmail.com>
2021-12-09 13:16:10 +0100

    Update Program.cs

--- PrzekletaRekurencja/Program.cs
+++ PrzekletaRekurencja/Program.cs
@@ -1,31 +1,37 @@
 ﻿using System;
-
 namespace PrzekletaRekurencja
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main3(string[] args)
         {
-
-            fun(5);
-            
-
-
+            fun(3);
             void fun(int n)
             {
                 if (n > 0)
                 {
                     fun(n - 1);
                     Console.WriteLine(n);
-
                     fun(n - 1);
                 }
-
             }
-
         }
+        static void Main()
+        {
+            fun(3);
+            void fun(int n)
+            {
+                int i = 0, j = i + 1;
 
-
+                for(i = 1;i<=n;i++)
+                {
+                    Console.WriteLine(i);
+                    for (;j<i;j++)
+                    {
+                        Console.WriteLine(j);
+                    }
+                }
+            }
+        }
     }
-
 }
