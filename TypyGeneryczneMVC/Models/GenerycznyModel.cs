11089b3439a0d106b1ee360ae7c754b307d909f0
krystianpetek <krystianpetek2@gmail.com>
2022-01-30 17:01:42 +0100

    up

---
+++ TypyGeneryczneMVC/Models/GenerycznyModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TypyGeneryczneMVC.Models
+{
+    public class GenerycznyModel<T>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<T> lista { get; set; }
+    }
+}
