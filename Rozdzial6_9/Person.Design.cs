3bd5407add3263beff70381e7d22a86791a68975
Krystian Petek <krystianpetek2@gmail.com>
2022-02-20 21:52:48 +0100

    Create Person.Design.cs

---
+++ Rozdzial6_9/Person.Design.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozdzial06_9
+{
+    partial class Person
+    {
+        partial void OnLastNameChanging(string value);
+        partial void OnFirstNameChanging(string value);
+
+        public string LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+            set
+            {
+                if((_LastName != value))
+                {
+                    OnLastNameChanging(value);
+                    _LastName = value;
+                }
+            }
+        }
+        private string _LastName;
+        public string FirstName
+        {
+            get
+            {
+                return _FirstName;
+            }
+            set
+            {
+                if((_FirstName != value))
+                {
+                    OnFirstNameChanging(value);
+                    _FirstName = value;
+                }
+            }
+        }
+        private string _FirstName;
+    }
+}
