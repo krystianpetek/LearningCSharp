2dbd5b5f13299c20e3e7ad89fc37ae391eb36497
Krystian Petek <krystianpetek2@gmail.com>
2022-02-28 23:26:56 +0100

    Update Program.cs

--- Rozdzial10_02/Program.cs
+++ Rozdzial10_02/Program.cs
@@ -7,25 +7,6 @@
             Coordinate coordinate1 = new Coordinate(new Longitude(48, 52), new Latitude(-2, -20));
         }
     }
-
-    // przeslanianie operatora == i !=
-    public sealed class ProductSerialNumber
-    {
-        public static bool operator ==(ProductSerialNumber leftHandSide, ProductSerialNumber rightHandSide)
-        {
-            if (leftHandSide is null)
-            {
-                return rightHandSide is null;
-            }
-            return leftHandSide.Equals(rightHandSide);
-        }
-
-        public static bool operator !=(ProductSerialNumber leftHandSide, ProductSerialNumber rightHandSide)
-        {
-            return !(leftHandSide == rightHandSide);
-        }
-    }
-
     public struct Latitude
     {
         public Latitude(int x, int y)
@@ -50,26 +31,52 @@
         public int Y { get; set; }
     }
 
-    internal struct Coordinate
+    public struct Coordinate
     {
-        public Latitude Latitude { get; }
-        public Longitude Longitude { get; }
+        public Latitude latitude { get; }
+        public Longitude longitude { get; }
 
         public Coordinate(Longitude x, Latitude y)
         {
-            Longitude = x;
-            Latitude = y;
+            longitude = x;
+            latitude = y;
         }
 
         public static Coordinate operator +(Coordinate source, Arc arc)
         {
-            Coordinate result = new Coordinate(new Longitude(source.Longitude.X + arc.LongitudeDifference.X, source.Longitude.Y + arc.LongitudeDifference.Y), new Latitude(source.Latitude.X, arc.LatitudeDifference.X));
+            Coordinate result = new Coordinate(new Longitude(source.longitude.X + arc.LongitudeDifference.X, source.longitude.Y + arc.LongitudeDifference.Y), new Latitude(source.latitude.X, arc.LatitudeDifference.X));
             return result;
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj is null)
+                return false;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            return Equals((Coordinate)obj);
+        }
+        public bool Equals(Coordinate? coordinate) => (latitude, longitude).Equals((coordinate?.longitude, coordinate?.latitude));
+
+        public static bool operator ==(Coordinate left, Coordinate right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Coordinate left, Coordinate right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return (longitude.X,longitude.Y,latitude.X,latitude.Y).GetHashCode();
         }
     }
 
     // dodawanie operatora +
-    internal struct Arc
+    public struct Arc
     {
         public Arc(Longitude longitudeDifference, Latitude latitudeDifference)
         {
@@ -79,5 +86,23 @@
 
         public Longitude LongitudeDifference { get; }
         public Latitude LatitudeDifference { get; }
+    }
+
+    // przeslanianie operatora == i !=
+    public sealed class ProductSerialNumber
+    {
+        public static bool operator ==(ProductSerialNumber leftHandSide, ProductSerialNumber rightHandSide)
+        {
+            if (leftHandSide is null)
+            {
+                return rightHandSide is null;
+            }
+            return leftHandSide.Equals(rightHandSide);
+        }
+
+        public static bool operator !=(ProductSerialNumber leftHandSide, ProductSerialNumber rightHandSide)
+        {
+            return !(leftHandSide == rightHandSide);
+        }
     }
 }
