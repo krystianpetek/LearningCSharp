bd0135631750d14d38e1123b28b1fe49b37287de
Krystian Petek <krystianpetek2@gmail.com>
2022-03-04 16:17:10 +0100

    Update Program.cs

--- Rozdzial11_1/Program.cs
+++ Rozdzial11_1/Program.cs
@@ -9,4 +9,17 @@ namespace Rozdzial11_1
             Console.WriteLine("Hello World!");
         }
     }
+    public sealed class TextNumberParser 
+    {
+        public static int Parse(string textDigit)
+        {
+            string[] digitTexts = { "zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć" };
+            int result = Array.IndexOf(digitTexts, textDigit ?? throw new ArgumentNullException(nameof(textDigit).ToLower()));
+            if(result < 0)
+            {
+                throw new ArgumentException("Argument nie reprezentuje cyfry", nameof(textDigit));
+            }
+            return result;
+        }
+    }
 }
