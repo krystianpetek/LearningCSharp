f44422206a2a8c1e5dfc8e4dfc7270fed50d6097
Krystian Petek <krystianpetek2@gmail.com>
2022-03-07 00:34:24 +0100

    ipo

--- Rozdzial12_2/Program.cs
+++ Rozdzial12_2/Program.cs
@@ -1,34 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace Rozdzial12_1
+namespace Rozdzial12_2
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             var x = new Program();
             Console.CursorVisible = false;
             x.Sketch();
         }
-
+        // TYPY GENERYCZNE
         public void Sketch()
         {
-            Stack<Cell> path = new Stack<Cell>();
+            Stack<Cell> path;
+            path = new Stack<Cell>();
             Cell currentPosition;
             ConsoleKeyInfo key;
+
             do
             {
-                key = Console.ReadKey(true);
-                switch (key.Key)
+                key = Console.ReadKey();
+                    switch(key.Key)
                 {
                     case ConsoleKey.Z:
-                        if (path.Count >= 1)
+                        if(path.Count > 0)
                         {
                             currentPosition = path.Pop();
+                            Console.SetCursorPosition(currentPosition.x, currentPosition.y);
+                            Undo();
                         }
                         break;
-
                     case ConsoleKey.UpArrow:
                         currentPosition = new Cell(Console.CursorLeft, Console.CursorTop - 1);
                         path.Push(currentPosition);
@@ -52,79 +54,44 @@ namespace Rozdzial12_1
                     default:
                         Console.Beep();
                         break;
-                }
 
+                }
                 Move(path);
+
             } while (key.Key != ConsoleKey.X);
+        
         }
 
-        public void Move(Stack<Cell> stos)
+        private void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Move(Stack<Cell> stos)
         {
             if (stos.Count > 0)
             {
                 Cell wartosc = stos.Peek();
-                if (wartosc.X < 0 || wartosc.Y < 0)
+                if (wartosc.x < 0 || wartosc.y < 0)
                 { Console.Beep(); }
                 else
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.Write(" ");
-                    Console.CursorLeft = wartosc.X;
-                    Console.CursorTop = wartosc.Y;
+                    Console.CursorLeft = wartosc.x;
+                    Console.CursorTop = wartosc.y;
                 }
             }
         }
     }
-
     public struct Cell
     {
-        public int X;
-        public int Y;
-
+        public int x;
+        public int y;
         public Cell(int x, int y)
         {
-            X = x;
-            Y = y;
+            this.x = x;
+            this.y = y;
         }
-    }
-
-    // typy bezposrednie z możliwą wartoscią null
-    internal struct NullableInt
-    {
-        /// <summary>
-        /// Udostępnia wartość, jeśli metoda HasValue zwraca true.
-        /// </summary>
-        public int Value { get; private set; }
-
-        /// <summary>
-        /// Określa czy wartość jest dostępna, czyh jest równa null.
-        /// </summary>
-        public bool HasValue { get; private set; }
-    }
-
-    internal struct NullableGuid
-    {
-        /// <summary>
-        /// Udostępnia wartość, jeśli metoda HasValue zwraca true.
-        /// </summary>
-        public Guid Value { get; private set; }
-
-        /// <summary>
-        /// Określa, czy wartość jest dostępna, czy jest równa null.
-        /// </summary>
-        public bool HasValue { get; private set; }
-    }
-
-    internal struct Nullable
-    {
-        /// <summary>
-        /// Udostępnia wartość, jeśli metoda HasValue zwraca true.
-        /// </summary>
-        public object Value { get; private set; }
-
-        /// <summary>
-        /// Określa, czy wartość jest dostępna, czy jest równa null.
-        /// </summary>
-        public bool HasValue { get; private set; }
     }
 }
---
+++ Rozdzial12_3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace Rozdzial12_3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var x = new Stack<Cell>(10);
+        }
+    }
+
+    public class Cell
+    {
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    // Prosta klasa generyczna
+    public class Stack<T>
+    {
+        public Stack(int maxSize)
+        {
+            InternalItems = new T[maxSize];
+        }
+
+        private T[] InternalItems { get; }
+
+        public void Push(T data)
+        { }
+
+        public T Pop()
+        { return InternalItems[InternalItems.Length - 1]; }
+    }
+
+    // generyczny interfejs
+    internal interface IPair<T>
+    {
+        T First { get; set; }
+        T Second { get; set; }
+    }
+
+    public struct Pair<T> : IPair<T>
+    {
+        public T First { get; set; }
+        public T Second { get; set; }
+    }
+
+    // wielokrotne implemenotwanie jednego interfejsu w tej samej klasie
+    public interface IContainer<T>
+    {
+        ICollection<T> items { get; set; }
+    }
+
+    internal class Email
+    {
+    }
+
+    internal class Phone
+    {
+    }
+
+    public class Address
+    {
+    }
+
+    public class Person : IContainer<Address>, IContainer<Phone>, IContainer<Email>
+    {
+        ICollection<Address> IContainer<Address>.items { get; set; }
+        ICollection<Phone> IContainer<Phone>.items { get; set; }
+        ICollection<Email> IContainer<Email>.items { get; set; }
+    }
+}
