a02ccc30f5c3de1210f36c4972dcb5fe65b48579
Krystian Petek <krystianpetek2@gmail.com>
2022-03-09 22:19:36 +0100

    Update Program.cs

--- Rozdzial12_5/Program.cs
+++ Rozdzial12_5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Rozdzial12_5
 {
@@ -6,7 +7,50 @@ namespace Rozdzial12_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        public struct Nullable<T> : IFormattable, IComparable, IComparable<Nullable<T>>, System.Data.SqlTypes.INullable where T : struct
+        {
+            //public static implicit operator T?(T value) => new T?(value);
+            //public static explicit operator T(T? value) => value!.Value;
+            public bool IsNull => throw new NotImplementedException();
+            public int CompareTo(object obj)
+            {
+                throw new NotImplementedException();
+            }
+            public int CompareTo(Nullable<T> other)
+            {
+                throw new NotImplementedException();
+            }
+            public string ToString(string format, IFormatProvider formatProvider)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+    }
+
+    // zestawy ograniczen, ograniczenia konstruktora
+    public class EntityDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TKey : IComparable<TKey>, IFormattable where TValue : EntityBase<TKey>, new()
+    {
+        public TValue MakeValue(TKey key)
+        {
+            TValue newEntity = new TValue();
+            {
+                newEntity.Key = key;
+            }
+            Add(newEntity.Key, newEntity);
+            return newEntity;
         }
     }
+    public class EntityBase <TKEY> where TKEY : notnull
+    {
+        public EntityBase(TKEY key)
+        {
+            Key = key;
+        }
+        public TKEY Key { get; set; }
+    }
+
+    //503
 }
