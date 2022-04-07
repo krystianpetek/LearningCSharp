0fe11f16d27653e119a2675da61e23063e9fdc40
Krystian Petek <krystianpetek2@gmail.com>
2022-04-07 13:58:00 +0200

    nauka idzie do przodu

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.Visibility = Visibility.Hidden;
+            btnStart.Opacity = 0.5;
+            MessageBox.Show("Witaj, świecie!");
+            btnStart.Opacity = 1;
+            btnStart.Visibility = Visibility.Visible;
+            btnStart.IsEnabled = false;
+        }
+
+        private void btnTime_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            btnTime.Content = date.ToString("T");
+        }
+
+        private void btnTime_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnTime.Content = "Czas";
+        }
+
+        private void btnWlaczStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = true;
+        }
+
+        private void btnPodwyzkaTak_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var marginTemp = btnPodwyzkaTak.Margin;
+            btnPodwyzkaTak.Margin = btnPodwyzkaNie.Margin;
+            btnPodwyzkaNie.Margin = marginTemp;
         }
     }
 }
+
+// frameworkelement i frameworkcontentelement
---
+++ Zadanie_3.01/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_3._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void lblBok_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txtBok.Focus();
+        }
+
+        private void lblPole_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPole.Focus();
+        }
+
+        private void lblObwod_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txtObwod.Focus();
+        }
+
+    }
+}
