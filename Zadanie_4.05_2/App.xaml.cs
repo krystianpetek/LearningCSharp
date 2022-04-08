eb84038bc577f3a70b3fcb29f4ca35ae5d3ecf07
Krystian Petek <krystianpetek2@gmail.com>
2022-04-09 00:12:18 +0200

    koniec na dzis nauka

---
+++ Zadanie_4.05_2/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Zadanie_4._05_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
