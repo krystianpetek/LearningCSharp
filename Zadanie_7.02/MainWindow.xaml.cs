c6a85cb5c79ed7fb4cf5ae974125d202defe611b
Krystian Petek <krystianpetek2@gmail.com>
2022-04-23 00:09:14 +0200

    Zadania od 7.1 do 7.4, ostatnie WIP, przyklady stosowania Grid => DataGrid

--- Zadanie_7.02/MainWindow.xaml.cs
+++ Zadanie_7.02/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +13,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.ObjectModel;
 
-namespace Zadanie_7._01
+namespace Zadanie_7._02
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<Produkt> ListaProduktow = null;
+        private ObservableCollection<Produkt> ListaProduktow;
 
         public MainWindow()
         {
             InitializeComponent();
             PrzygotujWiazanie();
         }
-
-        private void PrzygotujWiazanie()
+        
+        private void PrzygotujWiazanie() 
         {
             ListaProduktow = new ObservableCollection<Produkt>();
+
             ListaProduktow.Add(new Produkt("01-11", "ołówek", 8, "Katowice 1"));
             ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"));
             ListaProduktow.Add(new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"));
             ListaProduktow.Add(new Produkt("DZ-12", "długopis kulowy", 280, "Katowice 2"));
+            
             gridProdukty.ItemsSource = ListaProduktow;
         }
     }
---
+++ Zadanie_7.02/Produkt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7._02
+{
+    internal class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+        public Produkt() { }
+        public Produkt(string symbol, string nazwa, int liczbaSztuk, string magazyn)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            LiczbaSztuk = liczbaSztuk;
+            Magazyn = magazyn;
+        }
+        public override string ToString()
+        {
+            return $"{Symbol} {Nazwa} {LiczbaSztuk} {Magazyn}";
+        }
+    }
+}
