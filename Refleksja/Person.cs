00068570102b76a51fec76407eee7e30dfacf104
Krystian Petek <krystianpetek2@gmail.com>
2022-04-30 16:15:07 +0200

    nauka o refleksji

---
+++ Refleksja/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refleksja
+{
+    internal class Person
+    {
+        [DisplayProperty("First name")]
+        public string FirstName { get; set; }
+        [DisplayProperty("Last name")]
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+    }
+}
