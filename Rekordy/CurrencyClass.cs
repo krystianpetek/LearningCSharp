a47233cff0aac25d4991948c6979677895575b6b
Krystian Petek <krystianpetek2@gmail.com>
2022-05-02 15:53:20 +0200

    records in c# 10

---
+++ Rekordy/CurrencyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekords
+{
+    internal class CurrencyClass
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Value}";
+        }
+    }
+    
+}
