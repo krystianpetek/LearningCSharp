1cbf712709fe3e4ddb65414f88886309915411f0
Krystian Petek <krystianpetek2@gmail.com>
2022-05-07 11:56:41 +0200

    implementation factorymethod

---
+++ FactoryMethod/IBaseProduct.cs
@@ -0,0 +1,6 @@
+﻿namespace FactoryMethod;
+
+public interface IBaseProduct
+{
+    public void WykonajDzialanie();
+}
---
+++ FactoryMethod/ProductA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod;
+
+internal class ProductA : IBaseProduct
+{
+    public void WykonajDzialanie()
+    {
+        Console.WriteLine("Produkt A");
+    }
+}
