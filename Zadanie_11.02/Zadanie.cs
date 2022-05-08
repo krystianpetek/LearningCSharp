83e8095d3b434a73a1479369142b4f985d7fa7e2
Krystian Petek <krystianpetek2@gmail.com>
2022-05-08 12:50:41 +0200

    koniec rozdziału 11, bindowanie danych,  szablony danytch konwertery i szablopny kontrolek

---
+++ Zadanie_11.02/Zadanie.cs
@@ -0,0 +1,12 @@
+﻿namespace Zadanie_11._02;
+
+public class Zadanie
+{
+    public string Nazwa { get; set; }
+    public string Opis { get; set; }
+    public int Priorytet {get ; set; }
+    public override string ToString()
+    {
+        return $"({Nazwa}) {Opis} Priority:{Priorytet}";
+    }
+}
