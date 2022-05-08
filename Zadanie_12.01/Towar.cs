c2efae88aaa25caf186231aee26a260cec41e62e
Krystian Petek <krystianpetek2@gmail.com>
2022-05-08 17:55:45 +0200

    koniec zadan z rozdzialu 12, walidacja danych, bardzo wazne

---
+++ Zadanie_12.01/Towar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_12._01
+{
+    internal class Towar
+    {
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+    }
+}
---
+++ Zadanie_12.02/Towar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_12._02
+{
+    public class Towar : IDataErrorInfo
+    {
+        public string Nazwa { get; set; }
+        public double Cena { get; set; }
+
+        public string Error { get; }
+
+        public string this[string nazwaWlasciwosciTowaru]
+        {
+            get
+            {
+                string komunikat = string.Empty;
+                switch (nazwaWlasciwosciTowaru)
+                {
+                    case "Nazwa":
+                        if (string.IsNullOrEmpty(Nazwa))
+                            komunikat = "Nazwa musi być wpisana";
+                        else if (Nazwa.Length < 3)
+                            komunikat = "Nazwa musi mieć minimum 3 znaki!";
+                        break;
+                    case "Cena":
+                        if ((Cena < 0.1) || (Cena > 1000))
+                            komunikat = "Cena musi być z zakresu od 0,10 do 1000";
+                        break;
+                }
+
+                return komunikat;
+            }
+        }
+    }
+}
