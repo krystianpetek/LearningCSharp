a65b6315d040f0fe36fda3f7d90f87d95e3eed83
Krystian Petek <krystianpetek2@gmail.com>
2022-07-24 21:04:46 +0200

    Chapter 19 ended

--- Rozdzial19_1/Program.cs
+++ Rozdzial19_1/Program.cs
@@ -4,7 +4,8 @@ namespace Rozdzial19_1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static Stopwatch _Clock = new();
+        public async static Task Main(string[] args)
         {
             #region Invoking an Asynchronous Task (synchronous delegate)
             const int repetitions = 10000;
@@ -17,7 +18,7 @@ namespace Rozdzial19_1
             for (int count = 0; count < repetitions; count++)
                 Console.Write("+");
 
-            task.Wait();
+            await task;
             #endregion
 
             #region Polling a Task<T>
@@ -37,11 +38,105 @@ namespace Rozdzial19_1
             Console.WriteLine(taskPolling.Result);
 
             Trace.Assert(taskPolling.IsCompleted);
-            //829
+            #endregion
+
+            #region Calling Task.ContinueWith()
+            Console.WriteLine("Before");
+            Task taskA = Task.Run(
+                () => Console.WriteLine("Starting...")).ContinueWith(antecedent => Console.WriteLine("Continuing A..."));
+
+            Task taskB = taskA.ContinueWith(x => /* dont working */Task.Delay(10000)).ContinueWith(antecedent => Console.WriteLine("Continuing B..."));
+
+            Task taskC = taskA.ContinueWith(antecedent => Console.WriteLine("Continuing C..."));
+
+            Task.WaitAll(taskB, taskC);
+
+            Console.WriteLine("Finished!");
 
             #endregion
 
+            #region Registering for Notifications of Task Behavior with ContinueWith()
+            Task<string> taskR1 = Task.Run<string>(() => PiCalculator.Calculate(10));
 
+            Task faultedTaskR1 = taskR1.ContinueWith(antecedentTask =>
+            {
+                Trace.Assert(antecedentTask.IsFaulted);
+                Console.WriteLine("Task State: Faulted");
+            },
+            TaskContinuationOptions.OnlyOnFaulted);
+
+            Task canceledTaskR1 = taskR1.ContinueWith(antecedentTask =>
+            {
+                Trace.Assert(antecedentTask.IsCanceled);
+                Console.WriteLine("Task State: Canceled");
+            },
+            TaskContinuationOptions.OnlyOnCanceled);
+
+            Task completedTaskR1 = taskR1.ContinueWith(antecedentTask =>
+            {
+                Trace.Assert(antecedentTask.IsCompleted);
+                Console.WriteLine("Task State: Completed");
+            },
+            TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTaskR1.Wait();
+            #endregion
+
+            #region Handling a task's unhandled exception
+            Task taskHandling = Task.Run(() =>
+            {
+                throw new InvalidOperationException();
+            });
+
+            try
+            {
+                taskHandling.Wait();
+            }
+            catch (AggregateException exception)
+            {
+                exception.Handle(eachException =>
+                {
+                    Console.WriteLine($"ERROR: {eachException.Message}");
+                    return true;
+                });
+            }
+            #endregion
+
+            #region Registering for unhandled exception (in other thread)
+            try
+            {
+                _Clock.Start();
+                AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                {
+                    Message("Event handler starting");
+                    Delay(4000);
+                };
+
+                Thread thread = new Thread(() =>
+                {
+                    Message("Throwing exception");
+                    throw new Exception();
+                });
+                thread.Start();
+                Delay(2000);
+            }
+            finally
+            {
+                Message("Finally block running.");
+            }
+
+            static void Delay(int i)
+            {
+                Message($"Sleeping for {i} ms");
+                Thread.Sleep(i);
+                Message("Awake");
+            }
+
+            static void Message(string text)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}:{_Clock.ElapsedMilliseconds:F4}:{text}");
+            }
+            #endregion
         }
 
         #region PiCalculator
