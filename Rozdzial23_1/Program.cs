e1e41d063724c90fd19017b5b0f00779edf2c542
Krystian Petek <krystianpetek2@gmail.com>
2022-08-20 06:35:06 +0200

    end of long journey

---
+++ Rozdzial23_1/Program.cs
@@ -0,0 +1,77 @@
+﻿// CHAPTER 23: Platform Interoperability and Unsafe Code
+
+using System.Runtime.InteropServices;
+namespace Rozdzial23_1
+{
+    #region Declaring an External Method
+    class VirtualMemoryManager
+    {
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr GetCurrentProcess();
+
+        #region Declaring the VirtualAllocEx() API in C#
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            IntPtr dwSize,
+            AllocationType flAllocationType,
+            uint flProtect);
+        #endregion
+
+        #region using ref and out RATHER than pointers
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool VirtualProtectEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            IntPtr dwSize,
+            uint flNewProtect,
+            ref uint lpflOldProtect);
+        #endregion
+    }
+    #endregion
+
+    #region Declaring Types from Unmanaged Structs
+    [StructLayout(LayoutKind.Sequential)]
+    struct ColorRef
+    {
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+
+#pragma warning disable 414
+        private readonly byte Unused;
+#pragma warning restore 414;
+
+        public ColorRef(byte red, byte green, byte blue)
+        {
+            Blue = blue;
+            Green = green;
+            Red = red;
+            Unused = 0;
+        }
+    }
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Rozdzial23_2/Angle.cs
@@ -0,0 +1,20 @@
+﻿namespace Rozdzial23_2
+{
+    internal struct Angle
+    {
+        private readonly int v1;
+        private readonly int v2;
+        private readonly int v3;
+
+        public Angle(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public int Seconds { get; internal set; }
+        public int Minutes { get; internal set; }
+        public char[] Hours { get; internal set; }
+    }
+}
