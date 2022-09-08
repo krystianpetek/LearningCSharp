7925145762e38bc7ba3f69f5b1067022f857c00d
Krystian Petek <krystianpetek2@gmail.com>
2022-09-08 19:36:47 +0200

    Build a web app with Blazor

---
+++ Microsoft_Learn/10. Build web applications with Blazor/BlazorServerApp/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorServerApp.Models;
+
+public class TodoItem
+{
+    private static int _id = 1;
+    public TodoItem()
+    {
+        Id = _id;
+        _id++;
+    }
+
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; } = false;
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+}
