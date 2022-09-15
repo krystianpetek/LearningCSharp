1ccf1a956b931dd976990fe44f8b452ba5b1754f
Krystian Petek <krystianpetek2@gmail.com>
2022-09-15 23:10:58 +0200

    Build rich interactive components for your Blazor web apps

---
+++ Microsoft_Learn/14. Build rich interactive components for your Blazor web apps/BlazingPizzaSite/Controllers/OrdersController.cs
@@ -0,0 +1,69 @@
+﻿using BlazingPizzaSite.Data;
+using BlazingPizzaSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizzaSite.Controllers;
+
+[Route("orders")]
+[ApiController]
+public class OrdersController : Controller
+{
+    private readonly PizzaStoreContext _dbContext;
+    public OrdersController(PizzaStoreContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
+    {
+        var orders = await _dbContext.Orders
+            .Include(o => o.Pizzas)
+                .ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas)
+                .ThenInclude(p => p.Toppings)
+                .ThenInclude(t => t.Topping)
+            .OrderByDescending(o => o.CreatedTime)
+            .ToListAsync();
+
+        return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
+    }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderDetails(int orderId)
+    {
+        var order = await _dbContext.Orders
+            .Include(order => order.Pizzas)
+                .ThenInclude(pizza => pizza.Special)
+            .Include(order => order.Pizzas)
+                .ThenInclude(pizza => pizza.Toppings)
+                .ThenInclude(topping => topping.Topping)
+            .SingleOrDefaultAsync(x => x.OrderId == orderId);
+
+        if (order is null)
+            return NotFound();
+
+        var orderWithStatus = OrderWithStatus.FromOrder(order);
+        return Ok(orderWithStatus);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> PlaceOrder(Order order)
+    {
+        order.CreatedTime = DateTime.Now;
+
+        foreach (var pizza in order.Pizzas)
+        {
+            pizza.SpecialId = pizza.Special.Id;
+            pizza.Special = null;
+        }
+
+        _dbContext.Orders.Attach(order);
+        await _dbContext.SaveChangesAsync();
+
+        return order.OrderId;
+
+    }
+
+}
---
+++ Microsoft_Learn/14. Build rich interactive components for your Blazor web apps/BlazingPizzaSite/Program.cs
@@ -0,0 +1,84 @@
+using BlazingPizzaSite.Data;
+using BlazingPizzaSite.Services;
+using Microsoft.EntityFrameworkCore;
+using Topshelf;
+
+namespace BlazingPizzaSite;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var rc = HostFactory.Run(x =>
+        {
+            string path = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(path);
+            x.Service<StartApp>(s =>
+            {
+                s.ConstructUsing(host => new StartApp());
+                s.WhenStarted(async tc => await tc.Start());
+                s.WhenStopped(tc => tc.Stop());
+            });
+            x.RunAsLocalSystem();
+
+            x.SetDescription("BPS.Service");
+            x.SetDisplayName("BPS.Service");
+            x.SetServiceName("BPS.Service");
+        });
+
+        var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+        Environment.ExitCode = exitCode;
+    }
+}
+
+public class StartApp
+{
+    public void Stop()
+    {
+        Environment.Exit(0);
+    }
+
+    public async Task Start()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseIIS();
+        builder.WebHost.UseIISIntegration();
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddHttpClient();
+
+        // Database register
+        builder.Services.AddDbContext<PizzaStoreContext>(options => options.UseSqlite("Data Source=Data\\pizza.db"));
+
+        // Register services
+        builder.Services.AddSingleton<OrderState>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.MapRazorPages();
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
+
+        var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+        using var scope = scopeFactory.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+        if (db.Database.EnsureCreated())
+        {
+            SeedData.Initialize(db);
+        }
+
+        await app.RunAsync();
+    }
+}
