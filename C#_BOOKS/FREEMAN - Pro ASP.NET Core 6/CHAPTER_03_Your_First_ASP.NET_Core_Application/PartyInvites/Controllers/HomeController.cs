3efec24896d03fbf12c57fa84c86a2db606bcd77
Krystian Petek <krystianpetek2@gmail.com>
2022-09-22 18:20:36 +0200

    CHAPTER_03 - End (Data-entry application)

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_03_Your_First_ASP.NET_Core_Application/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+        if (ModelState.IsValid)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+        else
+        {
+            return View();
+        }
+    }
+
+    [HttpGet]
+    public ViewResult ListResponses()
+    {
+        return View("ListResponses", Repository.Responses.Where(x => x.WillAttend == true));
+    }
+}
