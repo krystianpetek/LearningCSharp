0ebf7806bdd114965a3b2a0b48e5290747d9f3ba
Krystian Petek <krystianpetek2@gmail.com>
2022-09-26 22:13:47 +0200

    CHAPTER_07 - SportsStore_A_Real_Application (END)
    Add bootstrap using libman, apply styling and split index into partial view

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_07-11_SportsStore_A_Real_Application/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_07-11_SportsStore_A_Real_Application/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -20,10 +20,12 @@ public class PageLinkTagHelper : TagHelper
     [ViewContext]
     [HtmlAttributeNotBound]
     public ViewContext? ViewContext { get; set; }
-    
     public PagingInfo? PageModel { get; set; }
-    
     public string? PageAction { get; set; }
+    public bool PageClassesEnabled { get; set; } = false;
+    public string PageClass { get; set; } = string.Empty;
+    public string PageClassNormal { get; set; } = string.Empty;
+    public string PageClassSelected { get; set; } = string.Empty;
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
@@ -35,6 +37,11 @@ public class PageLinkTagHelper : TagHelper
             {
                 var tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
