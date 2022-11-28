0e3ae5813815587fbb5fcb01f11c2e93ec325e76
Krystian Petek <krystianpetek2@gmail.com>
2022-11-28 22:07:24 +0100

    Add CartSummary component, Modify layout to add CartSummary component, used in page header, hidden cart summary layout when cart is displayed

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_07-11_SportsStore_A_Real_Application/SportsSln/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components;
+
+public class CartSummaryViewComponent : ViewComponent
+{
+    private readonly Cart _cartService;
+    
+    public CartSummaryViewComponent(Cart cartService)
+    {
+        _cartService = cartService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View("Default", _cartService);
+    }
+}
