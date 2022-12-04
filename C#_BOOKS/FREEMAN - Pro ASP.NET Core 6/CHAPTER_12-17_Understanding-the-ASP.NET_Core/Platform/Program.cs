17f0c3b9ed3489c8aa8b555097b42b10487b1087
Krystian Petek <krystianpetek2@gmail.com>
2022-12-04 07:12:20 +0100

    Added Custom Middleware using lambda with custom query check when its invoked, respond with custom message

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
@@ -6,39 +6,49 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// custom middleware when call to api via url https://localhost:7200?custom=true
+app.Use(async (context, next) =>
+{
+    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+    {
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Custom Middleware !" +
+            "\n");
+    }
+    await next();
+});
+
 
 // fetching request and response middleware
-app.Use(async (a, b) =>
+app.Use(async (httpContext, requestDelegate) =>
 {
-    Stream originalBodyStream = a.Response.Body;
+    Stream originalBodyStream = httpContext.Response.Body;
 
     try
     {
-        Console.WriteLine("before");
-        a.Request.EnableBuffering();
-        string bodyRequest = await new StreamReader(a.Request.Body).ReadToEndAsync();
-        a.Request.Body.Seek(0, SeekOrigin.Begin);
-
+        httpContext.Request.EnableBuffering();
+        string bodyRequest = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
+        httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
 
         var memoryFetchingStream = new MemoryStream();
-        a.Response.Body = memoryFetchingStream;
+        httpContext.Response.Body = memoryFetchingStream;
 
-        await b(a);
+        Console.WriteLine(bodyRequest);
+        Console.WriteLine($"Before middleware delegate: {bodyRequest}");
+
+        await requestDelegate(httpContext);
 
         memoryFetchingStream.Seek(0, SeekOrigin.Begin);
-
         string bodyResponse = await new StreamReader(memoryFetchingStream).ReadToEndAsync();
-
         memoryFetchingStream.Seek(0, SeekOrigin.Begin);
 
         await memoryFetchingStream.CopyToAsync(originalBodyStream);
 
-        Console.WriteLine(bodyResponse);
-        Console.WriteLine("after");
+        Console.WriteLine($"After middleware delegate: {bodyResponse}");
     }
     finally
     {
-        a.Response.Body = originalBodyStream;
+        httpContext.Response.Body = originalBodyStream;
     }
 });
 
