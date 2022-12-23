095a206938c380474e9360c10114db6bd13ca673
Krystian Petek <krystianpetek2@gmail.com>
2022-12-23 21:43:14 +0100

    Added ta middleware service implementation injected from DI in middleware constructor

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/CustomMiddleware/WeatherMiddleware.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/CustomMiddleware/WeatherMiddleware.cs
@@ -1,17 +1,21 @@
-﻿namespace Platform.CustomMiddleware;
+﻿using Platform.Services;
+
+namespace Platform.CustomMiddleware;
 
 public class WeatherMiddleware
 {
     private readonly RequestDelegate _requestDelegate;
+    private readonly IResponseFormatter _responseFormatter;
 
-    public WeatherMiddleware(RequestDelegate requestDelegate)
+    public WeatherMiddleware(RequestDelegate requestDelegate, IResponseFormatter responseFormatter)
     {
         _requestDelegate = requestDelegate;
+        _responseFormatter = responseFormatter;
     }
     public async Task InvokeAsync(HttpContext httpContext)
     {
         if (httpContext.Request.Path == "/middleware/class")
-            await httpContext.Response.WriteAsync($"Middleware class: It is raining in London.");
+            await _responseFormatter.Format(httpContext,$"Middleware class: It is raining in London. (WeatherMiddleware)");
         else
             await _requestDelegate(httpContext);
     }
