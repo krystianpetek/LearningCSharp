9059a7cdfd2552460498b6f21697d2f839c26726
Krystian Petek <krystianpetek2@gmail.com>
2022-12-25 06:34:09 +0100

    Accessing configuration data from appsettings and using it in Program.cs Added configuration data with Options pattern

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
@@ -1,4 +1,6 @@
+using Platform.CustomMiddleware;
 using Platform.Extensions;
+using Platform.MessageOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +11,31 @@ builder.Services.RegisterUrlRouting_Chapter13();
 
 builder.RegisterDependencyInjection_Chapter14();
 
+var servicesConfig = builder.Configuration; // use configuration settings to set up services
+builder.Services.Configure<MessageOption>(servicesConfig.GetSection("Location"));
 var app = builder.Build();
 
-// chapter 14
+// chapter 15 - configuration, environment
+app.MapGet("config", async (HttpContext httpContext, IConfiguration configuration) =>
+{
+    string? defaultDebug = configuration.GetRequiredSection("Logging:LogLevel").GetValue<string>("Default");
+    await httpContext.Response.WriteAsync($"The config setting is: {defaultDebug}");
+});
+
+var pipelineConfig = app.Configuration; // use configuration settings to set up pipeline
+app.UseMiddleware<LocationMiddleware>();
+
+app.Run();
+
+// chapter 14 - dependency injection
 app.DependencyInjectionMiddleware_Chapter14();
 app.Run();
 
-// chapter 13
+// chapter 13 - url routing
 app.UrlRoutingMiddleware_Chapter13();
 app.Run(); // prevents calling next middlewares, runs application and block the calling thread until
 
-// chapter 12
+// chapter 12 - middleware
 app.CustomMiddleware_Chapter12();
 app.MessageOptionsMiddleware_Chapter12();
 app.Run();
