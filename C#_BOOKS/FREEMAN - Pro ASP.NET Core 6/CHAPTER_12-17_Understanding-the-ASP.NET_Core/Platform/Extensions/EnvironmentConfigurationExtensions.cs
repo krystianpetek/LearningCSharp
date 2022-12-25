aa12c532c888965ee4ad64c905df33b848389443
Krystian Petek <krystianpetek2@gmail.com>
2022-12-25 11:20:50 +0100

    Chapter 15: Using the Platform Features (Completed)

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Extensions/EnvironmentConfigurationExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.FileProviders;
+using Platform.CustomMiddleware;
+using Platform.MessageOptions;
+
+namespace Platform.Extensions;
+
+public static class EnvironmentConfigurationExtensions
+{
+    public static IServiceCollection RegisterConfigurationEnvironment_Chapter15(this WebApplicationBuilder builder)
+    {
+        var servicesConfig = builder.Configuration; // use configuration settings to set up services
+        var serviceEnv = builder.Environment; // use environment to set up services
+
+        builder.Services.Configure<MessageOption>(servicesConfig.GetSection("Location"));
+
+        builder.Services.AddHttpLogging(options =>
+        {
+            options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+        });
+
+        return builder.Services;
+    }
+
+    public static IApplicationBuilder EnvironmentLoggingConfiguration_Chapter15(this WebApplication app)
+    {
+        app.UseHttpLogging();
+
+        ILogger logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+        logger.LogDebug("Pipeline configuration starting");
+
+        var pipelineConfig = app.Configuration; // use configuration settings to set up pipeline
+        var pipelineEnv = app.Environment; // use environment to set up pipeline
+
+        app.MapGet("config", async (HttpContext httpContext, IConfiguration configuration, IWebHostEnvironment env) => // https://localhost:7200/config
+        {
+            string? defaultDebug = configuration.GetRequiredSection("Logging:LogLevel").GetValue<string>("Default");
+            await httpContext.Response.WriteAsync($"The config setting is: {defaultDebug}");
+
+            string? environment = configuration["ASPNETCORE_ENVIRONMENT"];
+            string? envFromDI = env.EnvironmentName;
+            await httpContext.Response.WriteAsync($"\nThe env setting is: {envFromDI}");
+
+            string? wsId = configuration["WebService:Id"];
+            await httpContext.Response.WriteAsync($"\nThe secret ID is: {wsId}");
+        });
+
+        app.UseMiddleware<LocationMiddleware>();
+        app.MapGroup("chapter15").PopulationAPI(); // https://localhost:7200/chapter15/population
+
+        logger.LogDebug("Pipeline configuration complete");
+
+        app.UseStaticFiles(); // wwwroot
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider($"{pipelineEnv.ContentRootPath}/staticfiles"),
+            RequestPath = "/files"
+        });
+
+        return app;
+    }
+}
--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_12-17_Understanding-the-ASP.NET_Core/Platform/Program.cs
@@ -14,14 +14,7 @@ builder.Services.RegisterUrlRouting_Chapter13();
 
 builder.RegisterDependencyInjection_Chapter14();
 
-var servicesConfig = builder.Configuration; // use configuration settings to set up services
-var serviceEnv = builder.Environment; // use environment to set up services
-builder.Services.Configure<MessageOption>(servicesConfig.GetSection("Location"));
-
-builder.Services.AddHttpLogging(options =>
-{
-    options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
-});
+builder.RegisterConfigurationEnvironment_Chapter15();
 
 var app = builder.Build();
 
@@ -30,39 +23,7 @@ app.Map("/favicon.ico", delegate { }); // ignore favicon
 try
 {
     // chapter 15 - configuration, environment, logging
-    app.UseHttpLogging();
-
-    ILogger logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
-    logger.LogDebug("Pipeline configuration starting");
-
-    var pipelineConfig = app.Configuration; // use configuration settings to set up pipeline
-    var pipelineEnv = app.Environment; // use environment to set up pipeline
-
-    app.MapGet("config", async (HttpContext httpContext, IConfiguration configuration, IWebHostEnvironment env) => // https://localhost:7200/config
-    {
-        string? defaultDebug = configuration.GetRequiredSection("Logging:LogLevel").GetValue<string>("Default");
-        await httpContext.Response.WriteAsync($"The config setting is: {defaultDebug}");
-
-        string? environment = configuration["ASPNETCORE_ENVIRONMENT"];
-        string? envFromDI = env.EnvironmentName;
-        await httpContext.Response.WriteAsync($"\nThe env setting is: {envFromDI}");
-
-        string? wsId = configuration["WebService:Id"];
-        await httpContext.Response.WriteAsync($"\nThe secret ID is: {wsId}");
-    });
-
-    app.UseMiddleware<LocationMiddleware>();
-    app.MapGroup("chapter15").PopulationAPI(); // https://localhost:7200/chapter15/population
-
-    logger.LogDebug("Pipeline configuration complete");
-    var path = builder.Environment.ContentRootPath;
-    app.UseStaticFiles(); // wwwroot
-    app.UseStaticFiles(new StaticFileOptions
-    {
-        FileProvider = new PhysicalFileProvider($"{builder.Environment.ContentRootPath}/staticfiles"),
-        RequestPath = "/files"
-    });
-
+    app.EnvironmentLoggingConfiguration_Chapter15();
     app.Run();
 
     // chapter 14 - dependency injection
