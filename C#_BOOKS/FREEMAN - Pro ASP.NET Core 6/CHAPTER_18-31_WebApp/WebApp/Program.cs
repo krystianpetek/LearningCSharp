a36a3ff226e35d3ceb433280b18d0dbdb2ff49bb
Krystian Petek <krystianpetek2@gmail.com>
2022-12-31 13:33:26 +0100

    Chapter 20: Advanced Web Service Features - Completed

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Program.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using WebApp.Extensions;
 using WebApp.Middlewares;
 using WebApp.Models;
 using WebApp.Routes;
@@ -14,62 +15,25 @@ public static class Program
     public async static Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Pre21ChapterBuilder();
+        
         builder.Services.AddDbContext<DataContext>(options =>
         {
             var connectionString = builder.Configuration.GetConnectionString("ProductConnectionPgSql");
             options.UseNpgsql(connectionString);
             options.EnableSensitiveDataLogging(true);
         });
-        builder.Services.AddControllers().AddNewtonsoftJson().AddXmlSerializerFormatters();
-        builder.Services.AddCors(cors => cors.AddPolicy(name: "MyAllowSpecificOrigins", policy =>
-        {
-            policy.AllowAnyOrigin();
-        }));
-
-        //builder.Services.Configure<JsonOptions>(options =>
-        //{
-        //    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-        //    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        //});
-
-        builder.Services.Configure<MvcNewtonsoftJsonOptions>(options =>
-        {
-            options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-        });
-
-        builder.Services.Configure<MvcOptions>(options =>
-        {
-            options.ReturnHttpNotAcceptable = true;
-            options.RespectBrowserAcceptHeader = true;
-        });
-
-        builder.Services.AddSwaggerGen(swagger =>
-        {
-            swagger.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "WebApp"
-            });
-        });
 
         var app = builder.Build();
+        app.Pre21ChapterApp();
+        
         var dbContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
         dbContext.SeedDatabase();
 
-        app.UseMiddleware<TestMiddleware>();
-
-        app.MapProducts(ApiRoutes.MinProducts);
-
+        app.UseStaticFiles();
         app.MapControllers();
 
         app.MapGet("/", () => "Hello World!");
-
-        app.UseSwagger();
-        app.UseSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp");
-        });
 
         await app.RunAsync();
     }
