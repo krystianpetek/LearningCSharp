9ffb385a79cd9ef988c0a35dff7016b3eec3d3e4
Krystian Petek <krystianpetek2@gmail.com>
2023-01-29 09:57:23 +0100

    Added viewComponent CitySummary to MVC Home/Index view using InvokeAsync

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Components/CitySummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Components;
+
+public class CitySummary : ViewComponent
+{
+    private CitiesData _citiesData;
+
+    public CitySummary(CitiesData citiesData)
+    {
+        _citiesData = citiesData;
+    }
+
+    public string Invoke()
+    {
+        return $"{_citiesData.Cities.Count()} cities, " +
+            $"{_citiesData.Cities.Sum(city => city.Population)} people";
+    }
+}
