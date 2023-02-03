bd42112b838d01e434a467f090cbb29ee8feea60
Krystian Petek <krystianpetek2@gmail.com>
2023-02-03 22:01:41 +0100

    Added validation attributes in Product model Created custom property validation attribute Created custom model validation attribute in MVC and RazorPage

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Models/Product.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Models/Product.cs
@@ -2,27 +2,33 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using WebApp.Validation;
 
 namespace WebApp.Models;
 
+[PhraseAndPrice(Phrase = "Small", Price = "100")]
 public class Product
 {
     public long ProductId { get; set; }
-    
+
+    [Required(ErrorMessage = "Please enter a value")]
     public string Name { get; set; } = string.Empty;
     
     [Column(TypeName = "decimal(8,2)")]
     //[DisplayFormat(DataFormatString = "0:c2", ApplyFormatInEditMode = true)]
-    [BindNever]
+    [Range(1,999999,  ErrorMessage = "Please enter a positive price")]
+    //[BindNever]
     public decimal Price { get; set; }
-    
+
+    [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Category))]
     public long CategoryId { get; set; }
     
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Category? Category { get; set; }
-    
+
+    [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Supplier))]
     public long SupplierId { get; set; }
 
-    //[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Supplier? Supplier { get; set; }
 }
---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Validation/PrimaryKeyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Validation;
+
+public class PrimaryKeyAttribute : ValidationAttribute
+{
+    public Type? ContextType { get; set; }
+    public Type? DataType { get; set; }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if(ContextType != null && DataType != null)
+        {
+            DbContext? dbContext = validationContext.GetRequiredService(ContextType) as DbContext;
+
+            if (dbContext != null && dbContext.Find(DataType, value) == null)
+                return new ValidationResult(ErrorMessage ?? "Enter an existing key value");
+        }
+
+        return ValidationResult.Success;
+    }
+}
