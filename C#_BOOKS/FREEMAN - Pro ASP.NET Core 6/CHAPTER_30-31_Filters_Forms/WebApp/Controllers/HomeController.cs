8cafab06db58d470f873ffa9b4b68c1f29e646bf
Krystian Petek <krystianpetek2@gmail.com>
2023-02-04 22:19:13 +0100

    Created an authorization filter HttpsOnly to check if request is via https

--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_30-31_Filters_Forms/WebApp/Controllers/HomeController.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_30-31_Filters_Forms/WebApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Filters;
 
 namespace WebApp.Controllers;
 
 [RequireHttps]
+[HttpsOnly]
 public class HomeController : Controller
 {
     public IActionResult Index()
