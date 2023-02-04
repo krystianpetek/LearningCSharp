93b47f5edd44e00614df096b6ece2b2eab7a4a82
Krystian Petek <krystianpetek2@gmail.com>
2023-02-04 06:27:01 +0100

    Added feature remote validation to product, category and supplier

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Controllers/ValidationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValidationController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public ValidationController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet("productkey")]
+    public bool ProductKey(string productId)
+    {
+        return long.TryParse(productId, out long keyValue) && _dataContext.Products.Find(keyValue) != null;
+    }
+
+    [HttpGet("categorykey")]
+    public bool CategoryKey(string categoryId)
+    {
+        return long.TryParse(categoryId, out long keyValue) && _dataContext.Categories.Find(keyValue) != null;
+    }
+
+    [HttpGet("supplierkey")]
+    public bool SupplierKey(string supplierId)
+    {
+        return long.TryParse(supplierId, out long keyValue) && _dataContext.Suppliers.Find(keyValue) != null;
+    }
+}
--- C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Models/Product.cs
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_18-31_WebApp/WebApp/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
@@ -9,24 +10,27 @@ namespace WebApp.Models;
 [PhraseAndPrice(Phrase = "Small", Price = "100")]
 public class Product
 {
+    [Remote("ProductKey", "Validation", ErrorMessage = "Enter an existing key.")]
     public long ProductId { get; set; }
 
     [Required(ErrorMessage = "Please enter a value")]
     public string Name { get; set; } = string.Empty;
     
-    [Column(TypeName = "decimal(8,2)")]
     //[DisplayFormat(DataFormatString = "0:c2", ApplyFormatInEditMode = true)]
-    [Range(1,999999,  ErrorMessage = "Please enter a positive price")]
     //[BindNever]
+    [Column(TypeName = "decimal(8,2)")]
+    [Range(1,999999,  ErrorMessage = "Please enter a positive price")]
     public decimal Price { get; set; }
 
     [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Category))]
+    [Remote("CategoryKey", "Validation", ErrorMessage = "Enter an existing key.")]
     public long CategoryId { get; set; }
     
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Category? Category { get; set; }
 
     [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Supplier))]
+    [Remote("SupplierKey", "Validation", ErrorMessage = "Enter an existing key.")]
     public long SupplierId { get; set; }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
