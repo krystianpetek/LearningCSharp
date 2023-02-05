a2adc9bcb44f8238939dca1f80eded237608c07a
Krystian Petek <krystianpetek2@gmail.com>
2023-02-05 12:48:18 +0100

    Added SimpleCache filter but asynchronous this time

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_30-31_Filters_Forms/WebApp/Filters/SimpleCacheAsyncAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters;
+
+public class SimpleCacheAsyncAttribute : Attribute, IAsyncResourceFilter
+{
+    private readonly Dictionary<string, IActionResult> CachedResponses = new Dictionary<string, IActionResult>();
+    
+    public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+    {
+        PathString pathString = context.HttpContext.Request.Path;
+        if (CachedResponses.ContainsKey(pathString))
+        {
+            context.Result = CachedResponses[pathString];
+            CachedResponses.Remove(pathString);
+        }
+        else
+        {
+            ResourceExecutedContext execContext = await next();
+            if(execContext.Result != null)
+            {
+                CachedResponses.Add(pathString, execContext.Result);
+            }
+        }
+    }
+}
