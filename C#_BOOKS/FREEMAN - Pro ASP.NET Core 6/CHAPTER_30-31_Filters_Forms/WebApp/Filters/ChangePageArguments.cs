660f4d99478239d7ccf34f2fcccda87c32175900
Krystian Petek <krystianpetek2@gmail.com>
2023-02-05 16:29:40 +0100

    Added ActionFilter as Controller Filter Methods in HomeController Created ChangeArguments PageFilters and understand page filters executing

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_30-31_Filters_Forms/WebApp/Filters/ChangePageArguments.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters;
+
+public class ChangePageArguments : Attribute, IPageFilter
+{
+    public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+    {
+        // Method intentionally left empty.
+    }
+
+    public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+        if (context.HandlerArguments.ContainsKey("message1"))
+            context.HandlerArguments["message1"] = "New message from page filter attribute";
+    }
+
+    public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+    {
+        // Method intentionally left empty.
+    }
+}
