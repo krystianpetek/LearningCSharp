b6972294961d4dddb66b5064a9f5adfd0b04b0c0
Krystian Petek <krystianpetek2@gmail.com>
2023-02-07 22:01:45 +0100

    Added Form RazorPage with Products Added all other pages for CRUD operations using same PageModel but with different parameters

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_30-31_Filters_Forms/WebApp/Pages/EditorPageModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using WebApp.Models.ViewModels;
+
+namespace WebApp.Pages;
+
+public class EditorPageModel : PageModel
+{
+    public DataContext DataContext { get; set; }
+    public EditorPageModel(DataContext dataContext)
+    {
+        DataContext= dataContext;
+    }
+    public IEnumerable<Category> Categories => DataContext.Categories;
+    public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
+
+    public ProductViewModel? ViewModel { get; set; }
+}
