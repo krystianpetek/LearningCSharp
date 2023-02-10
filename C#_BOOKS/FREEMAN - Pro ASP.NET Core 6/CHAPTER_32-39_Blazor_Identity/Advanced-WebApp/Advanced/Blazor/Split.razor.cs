1e8ace097dcd7ef051bb82367945b51a6d5acbcd
Krystian Petek <krystianpetek2@gmail.com>
2023-02-10 06:36:48 +0100

    Using class to define RazorComponent to separate component code-behind @code section to other file by partial class Added component to Blazor.cshtml Added Component (CodeOnly.cs) by inherit from ComponentBase using only C# class

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_32-39_Blazor_Identity/Advanced-WebApp/Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,12 @@
+﻿using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor;
+
+public partial class Split
+{
+    [Inject]
+    public DataContext? DataContext { get; set; }
+
+    public IEnumerable<string> Names => DataContext?.People.Select(person => person.Firstname) ?? Enumerable.Empty<string>();
+}
