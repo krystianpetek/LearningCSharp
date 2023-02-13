d0d81c14ace61e027c8eb5406689c82ff3bf324e
Krystian Petek <krystianpetek2@gmail.com>
2023-02-13 06:14:11 +0100

    Chapter 36: Blazor forms and data completed Created DataController in Advanced

---
+++ C#_BOOKS/FREEMAN - Pro ASP.NET Core 6/CHAPTER_32-39_Blazor_Identity/Advanced-WebApp/Advanced/Controllers/DataController.cs
@@ -0,0 +1,80 @@
+﻿using Advanced.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advanced.Controllers;
+
+[ApiController]
+[Route("api/people")]
+public class DataController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public DataController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public IEnumerable<Person> GetAll()
+    {
+        IEnumerable<Person> people = _dataContext.People
+            .Include(person => person.Department)
+            .Include(person => person.Location);
+
+        foreach (Person person in people)
+        {
+            if (person.Department?.People != null)
+                person.Department.People = null;
+            if (person.Location?.People != null)
+                person.Location.People = null;
+        }
+
+        return people;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<Person> GetDetails(long id)
+    {
+        Person person = await _dataContext.People
+            .Include(person => person.Department)
+            .Include(person => person.Location)
+            .FirstOrDefaultAsync(person => person.PersonId == id);
+
+            if (person.Department?.People != null)
+                person.Department.People = null;
+            if (person.Location?.People != null)
+                person.Location.People = null;
+
+        return person;
+    }
+
+    [HttpPost]
+    public async Task Save([FromBody] Person person)
+    {
+        await _dataContext.People.AddAsync(person);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    [HttpPut]
+    public async Task Update([FromBody] Person p)
+    {
+        _dataContext.Update(p);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete(long id)
+    {
+        _dataContext.People.Remove(new Person() { PersonId = id });
+        await _dataContext.SaveChangesAsync();
+    }
+
+    [HttpGet("/api/locations")]
+    public IAsyncEnumerable<Location> GetLocations() =>
+        _dataContext.Locations.AsAsyncEnumerable();
+    
+    [HttpGet("/api/departments")]
+    public IAsyncEnumerable<Department> GetDepts() => 
+        _dataContext.Departments.AsAsyncEnumerable();
+}
